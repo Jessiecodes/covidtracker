9263289a01ac7ff6cd92170008cd9ee6ca60be4b
Jessiecodes <Jessak1202@gmail.com>
2020-06-10 15:14:20 -0400

    First commit

--- src/App.js
+++ src/App.js
@@ -1,26 +1,36 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+// import Cards from './components/Cards/Cards';
+// import Chart from './components/Chart/Chart';
+// import CountryPicker from './components/CountryPicker/CountryPicker'; or  import all components in one line like done beolw...  add into components folder an index.js file and default export all the components. 
+
+
+import { Cards, Chart, CountryPicker } from './components';
+import styles from './App.module.css';
+import { fetchData } from './api';
+
+class App extends React.Component {
+  state = {
+    data: {},
+  }
+
+  async componentDidMount () {
+    const fetcheddata = await fetchData();
+
+    this.setState({ data: fetcheddata});
+  }
+
+  render() {
+
+    const { data } = this.state;
+
+    return (
+      <div className={styles.container}>
+        <Cards data={data} />
+        <Chart />
+     
+      </div>
+    )
+  }
 }
-
 export default App;
