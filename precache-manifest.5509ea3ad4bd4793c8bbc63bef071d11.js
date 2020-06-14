6f9a8980bc074c8b135674fa19dee420cedc385b
Jessiecodes <Jessak1202@gmail.com>
2020-06-14 07:40:03 -0400

    Updates

---
+++ precache-manifest.5509ea3ad4bd4793c8bbc63bef071d11.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "92a49d299af3f795a98a450148d96802",
+    "url": "/covidtracker/index.html"
+  },
+  {
+    "revision": "2efff06ea5e0e089b035",
+    "url": "/covidtracker/static/css/main.5e7a944e.chunk.css"
+  },
+  {
+    "revision": "03c13f26222cb21f4ae5",
+    "url": "/covidtracker/static/js/2.53814798.chunk.js"
+  },
+  {
+    "revision": "176852ae27885dfd7559e0fb4b332d24",
+    "url": "/covidtracker/static/js/2.53814798.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "2efff06ea5e0e089b035",
+    "url": "/covidtracker/static/js/main.72e5d906.chunk.js"
+  },
+  {
+    "revision": "5a8d6b94eaaeb54901f5",
+    "url": "/covidtracker/static/js/runtime-main.89001081.js"
+  }
+]);
